fd84f71cd784229915224ab73f1961a7efa0549f
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2026-02-02 15:59:37 +0000

    Add 1-8

--- index.js
+++ index.js
@@ -12,21 +12,60 @@
 
 'use strict';
 
+// 2
+// Change the id method in index.js to return your student ID.
 function id() {
-  // e.g. return "UP654321";
-}
+  return "up2274439";
+};
 
+// 3
+// 3 Change the fn method in index.js to return your first name.
 function fn() {
-  return 'Replace This With Your First Name';
-}
+  return 'First';
+};
 
+// 4
+// Change the sn method in index.js to return your surname (family name).
 function sn() {
-  return 'Replace This With Your Surname';
-}
+  return 'Last';
+};
 
-function example() {
-  // replace this example with
-  // your first function then
-  // add more below as necessary.
+// 5
+// Create an add function that accepts two parameters called a and b, and returns their sum.
+function add(a, b) {
+  return a + b;
+};
 
-}
+// 6
+// Create a subtract function that accepts two parameters, a and b, and subtracts b from a and returns the result.
+function subtract(a, b) {
+  return a - b;
+};
+
+// 7
+// Create a checkObject function that accepts an object called obj as a parameter and sets the object's property `checked` to `true`.
+function checkObject(obj) {
+  obj.checked = true;
+  return obj;
+};
+
+// 8
+// Create a `checkObjectInside` function that accepts an object called `obj` as a parameter and if that object has an object value in its `data` property
+// it sets *that* inner object's property `checked` to `true`
+function checkObjectInside(obj) {
+  if (obj.hasOwnProperty("data")) {
+    obj.data.checked = true;
+  };
+  return obj;
+};
+
+// 9
+// Create an arraySet function that accepts three parameters `arr`, `i` and `n`.
+// The first one is an array and the second one an index.
+// The function should place the value of the third parameter into the array at an index specified by the second parameter,
+// if (and only if) such an index is already in the array. Note that your function does not need to return a value because the array is modified directly.
+// Create an arraySet function that accepts three parameters `arr`, `i` and `n`. The first one is an array and the second one an index. The function should place the value of the third parameter into the array at an index specified by the second parameter,
+// if (and only if) such an index is already in the array. Note that your function does not need to return a value because the array is modified directly.
+function arraySet(arr, i, n) {
+  let a = 1;
+};
